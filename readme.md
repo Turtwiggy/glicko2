b23026cc96f4f1764b586f6e56cef4e6aca05ae9
Turtwiggy <m-p-b@hotmail.co.uk>
2021-03-14 01:01:54 +0000

    first pass

---
+++ readme.md
@@ -0,0 +1,3 @@
+a rust impl of the glicko2 mmr system
+
+based off this paper http://www.glicko.net/glicko/glicko2.pdf
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// https://en.wikipedia.org/wiki/Glicko_rating_system
+// https://csgo-ranks.com/elo/
+// http://www.glicko.net/glicko/glicko2.pdf
+
+// aims for project:
+// a command line tool for a persistent mmr ranking system
+// where you can enter teams of 5v5, and calculate elo change in all of them
+// extention: vizulize changes
+
+// every player in glicko2 has
+// -- a rating, r, a rating devation, RD
+// -- a rating volitility, rv
+// volitility measure indicates the degree of expected fluctiaton in a player's rating
+
+fn main() {
+    println!("Hello, world!");
+}
